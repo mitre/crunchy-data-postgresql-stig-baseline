cca7d96113dd55542f3301af24eec905d24abea6
Eugene Aronne <34140975+ejaronne@users.noreply.github.com>
2022-11-01 21:30:15 -0400

    Update V-233584.rb

--- controls/V-233584.rb
+++ controls/V-233584.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 
 control	'V-233584' do
-	title	"PostgreSQL must use NSA-approved cryptography to protect classified information in accordance with the 
+	title	"PostgreSQL must use #{input('org_name')[:acronym]}-approved cryptography to protect classified information in accordance with the 
 	data owner’s requirements."
 	desc	"Use of weak or untested encryption algorithms undermines the purposes of utilizing encryption to protect 
 	data. The application must implement cryptographic modules adhering to the higher standards approved by the 
@@ -10,12 +10,12 @@ control	'V-233584' do
 It is the responsibility of the data owner to assess the cryptography requirements in light of applicable federal 
 laws, Executive Orders, directives, policies, regulations, and standards.
 
-NSA-approved cryptography for classified networks is hardware based. This requirement addresses the compatibility of 
+#{input('org_name')[:acronym]}-approved cryptography for classified networks is hardware based. This requirement addresses the compatibility of 
 PostgreSQL with the encryption devices."
 	desc	'rationale', ''
 	desc	'check', "If PostgreSQL is deployed in an unclassified environment, this is not applicable (NA).
 
-If PostgreSQL is not using NSA-approved cryptography to protect classified information in accordance with applicable 
+If PostgreSQL is not using #{input('org_name')[:acronym]}-approved cryptography to protect classified information in accordance with applicable 
 federal laws, Executive Orders, directives, policies, regulations, and standards, this is a finding.
 
 To check if PostgreSQL is configured to use SSL, as the database administrator (shown here as \"postgres\"), run the 
@@ -26,7 +26,7 @@ $ psql -c \"SHOW ssl\"
 
 If SSL is off, this is a finding.
 
-Consult network administration staff to determine whether the server is protected by NSA-approved encrypting devices. 
+Consult network administration staff to determine whether the server is protected by #{input('org_name')[:acronym]}-approved encrypting devices. 
 If not, this a finding."
 	desc	'fix', "Note: The following instructions use the PGDATA and PGVER environment variables. See 
 	supplementary content APPENDIX-F for instructions on configuring PGDATA and APPENDIX-H for PGVER.
@@ -46,7 +46,7 @@ $ sudo systemctl reload postgresql-${PGVER?}
 
 For more information on configuring PostgreSQL to use SSL, see supplementary content APPENDIX-G.
 
-Deploy NSA-approved encrypting devices to protect the server on the network."
+Deploy #{input('org_name')[:acronym]}-approved encrypting devices to protect the server on the network."
 	impact 0.5
 	tag severity: 'medium'
   tag gtitle: 'SRG-APP-000416-DB-000380'
