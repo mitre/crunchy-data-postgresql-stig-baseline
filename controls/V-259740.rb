448cddd8508760ca59bba5b3698627bc9c63b8d4
Daniel Medina <dmedina@mitre.org>
2024-02-09 14:51:16 -0500

    implement new control. Create new input to specify for latest organization allowed postgres version in inspec.yml.

--- controls/V-259740.rb
+++ controls/V-259740.rb
@@ -41,4 +41,19 @@ Upgrade unsupported DBMS or unsupported components to a supported version of the
   tag 'documentable'
   tag cci: ['CCI-003376']
   tag nist: ['SA-22 a']
+
+  allowed_postgres_version = input('org_allowed_postgres_version')
+  installed_postgres_version = command('psql --version').stdout.split[2]
+
+  if (allowed_postgres_version.nil? || allowed_postgres_version.empty?)
+    describe "Your installed Postgres version is: #{installed_postgres_version}. You must review this control manually or set / pass the 'org_allowed_postgres_version' to the profile. The latest release can be found at http://www.postgresql.org/support/versioning/" do
+      skip "Your installed Postgres version is: #{installed_postgres_version}. You must review this control manually or set / pass the 'org_allowed_postgres_version' to the profile. The latest release can be found at http://www.postgresql.org/support/versioning/"
+    end
+  else  
+    describe 'PostgreSQL installed version' do
+      subject { installed_postgres_version }
+      it { should cmp >= allowed_postgres_version }
+    end
+  end
+
 end
